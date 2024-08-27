4824bb585d403c825a2b77accd5dfe0fce2f1234
Dell <Dell@DESKTOP-GSS5CVG>
2024-08-27 12:32:10 +0530

    commit and push

---
+++ QALegendProject/src/main/java/constants/Constants.java
@@ -0,0 +1,17 @@
+package constants;
+
+public class Constants {
+	public static final String TESTDATA_EXCELPATH = "\\src\\test\\resources\\TestData.xlsx";
+	public static final String HOME_DIRECTORY = System.getProperty("user.dir");
+	public static final String LOGIN_PAGE = "LoginPage";
+	public static final String USERMANAGEMENT_PAGE = "UserManagementPage";
+	public static final String FORGOTPASSWORD_PAGE = "PasswordResetPage";
+	public static final String EMAIL_RANDOMDATADOT = ".";
+	public static final String EMAIL_RANDOMDATASUFFIX = "@yahoo.com";
+	public static final String USERNAME_RANDOMDATASUFFIX = "@1";
+	public static final String WELCOMEMESSAGE_EXPECTEDPREFIX = "Welcome";
+	public static final String WELCOMEMESSAGE_EXPECTEDSUFFIX = ",";
+	
+	
+
+}
